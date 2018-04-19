3c0ba5b6847cd724f497320608c5c289f8dc23dc
leanfj <leanfj@hotmail.com>
2018-04-19 18:13:36 -0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var tinify = require('gulp-tinify');
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function () {
@@ -22,6 +23,13 @@ gulp.task('sass', function () {
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest("./css"))
         .pipe(browserSync.stream());
+});
+
+//minificar imagens
+gulp.task('tinify', function () {
+    gulp.src('./mini-img/**/*')
+        .pipe(tinify('b_wi1hUvhf7dZxFcK0vZT3AUTckgfbTi'))
+        .pipe(gulp.dest('./img'));
 });
 
 gulp.task('default', ['serve']);
